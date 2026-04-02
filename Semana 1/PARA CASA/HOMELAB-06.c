155d6954396a57d98c8f5bac7168b8d90998eb69
tkusal <thiago.kusal@outlook.com>
2026-04-01 22:34:06 -0300

    Import da stdio.h

--- Semana 1/PARA CASA/HOMELAB-06.c
+++ Semana 1/PARA CASA/HOMELAB-06.c
@@ -3,6 +3,8 @@
     Apresente então o maior valor lido e a posição (de 1 a 5) dentre os valores lidos em que ele foi encontrado.
 */
 
+#include <stdio.h>
+
 int main() {
     int valor, maior_valor, posicao;
 
