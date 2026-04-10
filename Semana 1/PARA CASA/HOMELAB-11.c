52a99fcb7b38b73d011abf00afcb3e41fedd34ce
tkusal <thiago.kusal@outlook.com>
2026-04-09 21:59:50 -0300

    Ajustes e Gabarito

--- Semana 1/PARA CASA/HOMELAB-11.c
+++ Semana 1/PARA CASA/HOMELAB-11.c
@@ -16,3 +16,47 @@
 
     Para simplificar e não haver necessidade de tratar um empate, vamos considerar um mundo perfeito onde não existem empates.
 */
+
+#include <stdio.h>
+
+int main() {
+    int voto;
+    int alfa = 0, beta = 0, omega = 0, branco = 0, nulo = 0;
+
+    for (int i = 1; i <= 10; i++) {
+        printf("Eleitor %d - Digite seu voto: ", i);
+        scanf("%d", &voto);
+
+        if (voto == 1) {
+            alfa++;
+        } else if (voto == 2) {
+            beta++;
+        } else if (voto == 3) {
+            omega++;
+        } else if (voto == 4) {
+            branco++;
+        } else if (voto == 5) {
+            nulo++;
+        } else {
+            printf("Voto Invalido\n");
+            i--;
+        }
+    }
+
+    printf("\nResultado:\n");
+    printf("Chapa Alfa: %d\n", alfa);
+    printf("Chapa Beta: %d\n", beta);
+    printf("Chapa Omega: %d\n", omega);
+    printf("Brancos: %d\n", branco);
+    printf("Nulos: %d\n", nulo);
+
+    if (alfa > beta && alfa > omega) {
+        printf("Vencedor: Chapa Alfa\n");
+    } else if (beta > alfa && beta > omega) {
+        printf("Vencedor: Chapa Beta\n");
+    } else {
+        printf("Vencedor: Chapa Omega\n");
+    }
+
+    return 0;
+}
--- Semana 1/PARA CASA/HOMELAB-12.c
+++ Semana 1/PARA CASA/HOMELAB-12.c
@@ -6,3 +6,29 @@
 
     Escreva um programa que calculo o valor final para este estagiário.
 */
+
+#include <stdio.h>
+
+int main() {
+    float C, i, M;
+    int t;
+
+    printf("Digite o capital inicial: ");
+    scanf("%f", &C);
+
+    printf("Digite a taxa de juros (ex: 0.05 para 5%%): ");
+    scanf("%f", &i);
+
+    printf("Digite o tempo (em meses): ");
+    scanf("%d", &t);
+
+    M = C;
+
+    for (int j = 1; j <= t; j++) {
+        M = M * (1 + i);
+    }
+
+    printf("Valor final: %.2f\n", M);
+
+    return 0;
+}
